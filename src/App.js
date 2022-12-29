ef668d9d5a4301be3161b8f7fec5b2f36bb792d3
JackelineJ <jacky@rustningshield.com>
2022-12-28 21:50:41 -0300

    import del navbar y itemlist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import ItemList from './components/ItemList';
+
 
 function App() {
+  const greetingComponent = 'Hello!';
+  const pageLogo = 'queenlogo.png';
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img className='queenLogo' src={pageLogo} alt='queen logo'/>
+        <NavBar />
+        <ItemList greeting={greetingComponent}/>
       </header>
     </div>
   );
