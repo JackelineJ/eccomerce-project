6fe08a0f5b34b4032a4d7547ade927dd70b4b567
JackelineJ <jacky@rustningshield.com>
2023-01-20 00:06:24 -0300

    elimine import de itemcount

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,11 +1,9 @@
-import ItemCount from '../ItemCount/ItemCount';
 import './ItemList.css';
 
 const ItemList = (props) => {
     return (
         <div className='Container'>
             <h1>{props.greeting} Pick what you like best</h1>
-            <ItemCount />
         </div>
     )
 }
